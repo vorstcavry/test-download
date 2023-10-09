ccbe539f5fe1029a2579c85e8e461b2e6021b39c
Bagus Vendi Vannessa <vorstcavry@gmail.com>
2023-10-09 21:56:07 +0700

    Update install.py

--- install.py
+++ install.py
@@ -3,7 +3,7 @@ import platform
 
 if platform.system() == "Windows":
     if not launch.is_installed("gdown"):
-        launch.run_pip("install gdown", "requirements for Batchlinks Download extension")
+        launch.run_pip("install gdown", "requirements for Downloader")
 
     if not launch.is_installed("wget"):
-        launch.run_pip("install wget", "requirements for Batchlinks Download extension")
+        launch.run_pip("install wget", "requirements for Downloader")
